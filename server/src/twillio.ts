e44bf637bf61095b6f758084657a6be5d7bba775
Chris Woodle <chris@droplit.io>
2019-04-01 02:00:48 -0400

    added twilio

---
+++ server/src/twillio.ts
@@ -0,0 +1,23 @@
+const accountSid = process.env.TWILIO_AUTH_TOKEN;
+const authToken = process.env.TWILIO_ACCOUNT_SID;
+const fromNumber = process.env.TWILIO_FROM_NUMBER;
+
+import * as twilio from 'twilio';
+const client = twilio(accountSid, authToken);
+
+export function call(to: string) {
+  return client.calls
+    .create({
+      url: 'https://woodle.ngrok.io/call.xml',
+      to: `+${to}`,
+      from: `+${fromNumber}`
+    });
+}
+export function sms(message: string, to: string) {
+  return client.messages
+    .create({
+      body: message,
+      from: `+${fromNumber}`,
+      to: `+${to}`,
+    });
+}
